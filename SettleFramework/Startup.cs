b6cce3623bf32f21f25da69854e188880419480f
ccestari <cestarimail@gmail.com>
2017-01-27 20:54:55 -0500

    Add project files.

---
+++ SettleFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SettleFramework.Startup))]
+namespace SettleFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
